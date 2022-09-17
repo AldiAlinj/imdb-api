6fd434e764bfbc733e79f1292c965d4589aaf584
Aldi <aldialinj0@gmail.com>
2022-09-17 23:34:00 +0200

    Finished filtering

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,170 +1,218 @@
-import React, { useEffect, useState } from 'react'
-import { useSelector, useDispatch } from 'react-redux'
-import { fetchAsyncMovies, getLoading, getMovies } from '../redux/movieSlice';
-import 'react-calendar/dist/Calendar.css';
+import React, { useEffect, useState } from "react";
+import { useSelector, useDispatch } from "react-redux";
+import { fetchAsyncMovies, getLoading, getMovies } from "../redux/movieSlice";
+import "react-calendar/dist/Calendar.css";
 
 const Home = () => {
+  const dispatch = useDispatch();
+  const movies = useSelector(getMovies);
+  const loading = useSelector(getLoading);
 
-    const dispatch = useDispatch();
-    const movies = useSelector(getMovies)
-    const loading = useSelector(getLoading)
-    
-    const genres = [
-      'Action',
-      'Adventure',
-      'animation',
-      'Biography',
-      'Comedy',
-      'Crime',
-      'Documentary',
-      'Drama',
-      'Family',
-      'Fantasy',
-      'Film-Noir',
-      'Game-Show',
-      'History',
-      'Horror',
-      'Music',
-      'Musical',
-      'Mystery',
-      'News',
-      'Reality-Tv',
-      'Romance',
-      'Sci-Fi',
-      'Sport',
-      'Talk-Show',
-      'Thriler',
-      'War',
-    ]
+  const genres = [
+    "Action",
+    "Adventure",
+    "animation",
+    "Biography",
+    "Comedy",
+    "Crime",
+    "Documentary",
+    "Drama",
+    "Family",
+    "Fantasy",
+    "Film-Noir",
+    "Game-Show",
+    "History",
+    "Horror",
+    "Music",
+    "Musical",
+    "Mystery",
+    "News",
+    "Reality-Tv",
+    "Romance",
+    "Sci-Fi",
+    "Sport",
+    "Talk-Show",
+    "Thriler",
+    "War",
+  ];
 
-    
+  const [parameters, setParameters] = useState({
+    title: "",
+    genre: "",
+    rating: "",
+    startDate: "",
+    endDate: "",
+  });
 
-
-    const [parameters, setParameters] = useState({
-      search: '',
-      genre: ',',
-      rating: ',',
-      startDate: '',
-      endDate: ''
-    })
-    
-   
-    const searchMovies = () => {
-
-      const [day, month, year] = parameters.startDate.split('-')
-      const formatedStartDate = [year, month, day].join('-')
-
-      setParameters({...parameters, startDate: formatedStartDate})
-
-      console.log(parameters.startDate);
-
-
-      // if(parameters.startDate !== '' && parameters.endDate == ''){
-      //   alert('Please enter an end date')
-      // }else if(parameters.startDate == '' && parameters.endDate !== ''){
-      //   alert('Please enter a start date!')
-      // }else{
-      //   dispatch(fetchAsyncMovies(parameters))
-      // }
+  const searchMovies = () => {
+    if (parameters.startDate !== "" && parameters.endDate === "") {
+      alert("Please enter an end date");
+    } else if (parameters.startDate === "" && parameters.endDate !== "") {
+      alert("Please enter a start date!");
+    } else {
+      dispatch(fetchAsyncMovies(parameters));
     }
-  
 
-    useEffect(() => {
-      
-        dispatch(fetchAsyncMovies(parameters))
-    
-    }, [dispatch])
+    setParameters({
+      search: "",
+      genre: "",
+      rating: "",
+      startDate: "",
+      endDate: "",
+    });
+  };
 
+  useEffect(() => {
+    dispatch(fetchAsyncMovies(parameters));
+  }, [dispatch]);
 
-    
-    
   return (
-    loading ? (
-      <div>
-        loading
-      </div>
-
-    )
-    : (
-      <div className='container'>
+    <div className="container">
       <div className="row">
         <div className="col-12">
           <h1>IMDB API</h1>
         </div>
       </div>
-        <div className="col-12">
-          <h1>Filter Movies</h1>
-        </div>
+      <div className="col-12">
+        <h1>Filter Movies</h1>
+      </div>
       <div className="row justify-content-center align-items-center">
         <div className="col-2">
-        <label for="basic-url" class="form-label">Title</label>
+          <label for="basic-url" class="form-label">
+            Title
+          </label>
           <div class="input-group mb-3">
-            <input type="text" class="form-control" id="search" placeholder='Search...' onChange={(title) => setParameters({...parameters, search: title}) } />
+            <input
+              type="text"
+              class="form-control"
+              id="search"
+              placeholder="Search..."
+              onChange={(event) =>
+                setParameters({ ...parameters, title: event.target.value })
+              }
+            />
           </div>
         </div>
         <div className="col-2">
-        <label for="basic-url" class="form-label">Rating</label>
-        <div class="input-group mb-3">
-          <select class="form-select" id="rating" onChange={(rating) => setParameters({...parameters, rating: rating})}>
-            <option selected>Choose...</option>
-            <option value="1">One star</option>
-            <option value="2">Two stars</option>
-            <option value="3">Three stars</option>
-            <option value="4">Four stars</option>
-            <option value="5">Five stars</option>
-          </select>
-        </div>
+          <label for="basic-url" class="form-label">
+            Rating
+          </label>
+          <div class="input-group mb-3">
+            <select
+              class="form-select"
+              id="rating"
+              onChange={(event) =>
+                setParameters({ ...parameters, rating: event.target.value })
+              }
+            >
+              <option selected>Choose...</option>
+              <option value="1.0">One star</option>
+              <option value="2.0">Two stars</option>
+              <option value="3.0">Three stars</option>
+              <option value="4.0">Four stars</option>
+              <option value="5.0">Five stars</option>
+            </select>
+          </div>
         </div>
         <div className="col-2">
-        <label for="basic-url" class="form-label">Genre</label>
-        <div class="input-group mb-3">
-          <select class="form-select" id="genre" onChange={(genre) => setParameters({...parameters, genre: genre})}>
-            <option selected>Choose...</option>
-            {genres.map((genre) => (
-            <option value={genre.toLowerCase()}>{genre}</option>
-
+          <label for="basic-url" class="form-label">
+            Genre
+          </label>
+          <div class="input-group mb-3">
+            <select
+              class="form-select"
+              id="genre"
+              onChange={(event) =>
+                setParameters({ ...parameters, genre: event.target.value })
+              }
+            >
+              <option selected>Choose...</option>
+              {genres.map((genre) => (
+                <option value={genre.toLowerCase()}>{genre}</option>
+              ))}
+            </select>
+          </div>
+        </div>
+        <div className="col-2">
+          <label for="basic-url" class="form-label">
+            Starting Year
+          </label>
+          <div class="input-group mb-3 d-flex justify-content-center align-items-center">
+            <label className="me-2" htmlFor="startDate">
+              From{" "}
+            </label>
+            <input
+              type="date"
+              class="form-control"
+              id="startDate"
+              onChange={(event) =>
+                setParameters({ ...parameters, startDate: event.target.value })
+              }
+            />
+          </div>
+        </div>
+        <div className="col-2">
+          {parameters.startDate === "" ? null : (
+            <>
+              <label for="basic-url" class="form-label">
+                Ending Year
+              </label>
+              <div class="input-group mb-3 d-flex justify-content-center align-items-center">
+                <label className="me-2" htmlFor="endDate">
+                  To{" "}
+                </label>
+                <input
+                  type="date"
+                  class="form-control"
+                  min={parameters.startDate}
+                  id="endDate"
+                  onChange={(event) =>
+                    setParameters({
+                      ...parameters,
+                      endDate: event.target.value,
+                    })
+                  }
+                />
+              </div>
+            </>
+          )}
+        </div>
+        <div className="col-2">
+          <button type="button" class="btn btn-primary" onClick={searchMovies}>
+            Search
+          </button>
+        </div>
+      </div>
+      {loading ? (
+        <div>loading...</div>
+      ) : (
+        <div className="container-fluid">
+          <div className="row gy-3">
+            {movies.results.map((movie) => (
+              <div className="col-4 d-flex justify-content-center align-items-center">
+                <div class="card" style={{ width: "18rem" }}>
+                  <img
+                    src={movie.image}
+                    width={250}
+                    height={400}
+                    class="card-img-top"
+                    alt="..."
+                  />
+                  <div class="card-body">
+                    <h5 class="card-title fs-5">{movie.title}</h5>
+                    <p class="card-text">{movie.plot}</p>
+                    <a href="" class="btn btn-primary">
+                      Go to movie
+                    </a>
+                  </div>
+                </div>
+              </div>
             ))}
-          </select>
+          </div>
         </div>
-        </div>
-        <div className="col-2">
-        <label for="basic-url" class="form-label">Starting Year</label>
-        <div class="input-group mb-3 d-flex justify-content-center align-items-center">
-          <label htmlFor="startDate">From</label>
-           <input type="date" class="form-control" id="startDate" onChange={(startDate) => setParameters({...parameters, startDate: startDate})} />
-        </div>
-        </div>
-        <div className="col-2">
-        <label for="basic-url" class="form-label">Ending Year</label>
-        <div class="input-group mb-3 d-flex justify-content-center align-items-center">
-          <label htmlFor="endDate">To</label>
-           <input type="date" class="form-control" min={parameters.startDate} id="endDate" onChange={(endDate) => setParameters({...parameters, endDate: endDate})} />
-        </div>
-        </div>
-        <div className="col-2">
-        <button type="button" class="btn btn-primary" onClick={searchMovies}>Search</button>
-        </div>
-       
-      </div>
-    <div className="container-fluid">
-      <div className="row gy-3">
-       {movies.results.map((movie) => (
-         <div className="col-4 d-flex justify-content-center align-items-center">
-         <div class="card" style={{width: '18rem'}}>
-           <img src={movie.image} width={250} height={400} class="card-img-top" alt="..." />
-           <div class="card-body">
-             <h5 class="card-title fs-5">{movie.title}</h5>
-             <p class="card-text">{movie.plot}</p>
-             <a href="" class="btn btn-primary">Go to movie</a>
-           </div>
-         </div>
-         </div>
-       ))}
-      </div>
+      )}
     </div>
-    </div>
-    )
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
--- src/redux/movieSlice.js
+++ src/redux/movieSlice.js
@@ -1,47 +1,42 @@
 import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
 import axios from "axios";
 
-
 export const fetchAsyncMovies = createAsyncThunk(
-    'movies/fetchAsyncMovies',
-    async(parameters) => {
-        const url = `https://imdb-api.com/API/AdvancedSearch/k_8wgipecq?groups=top_250&count=250&release_date=${parameters.startDate},${parameters.endDate}&genres=${parameters.genre}&user_rating=${parameters.rating}`
-        const response = await axios.get(url)
+  "movies/fetchAsyncMovies",
+  async (parameters) => {
+    const url = `https://imdb-api.com/API/AdvancedSearch/k_5l07gzzx?groups=top_250&count=250&release_date=${parameters.startDate},${parameters.endDate}&genres=${parameters.genre},&user_rating=${parameters.rating},&title=${parameters.title}`;
+    console.log(url);
+    const response = await axios.get(url);
 
-        return response.data
-    }
-)
-
+    return response.data;
+  }
+);
 
 const initialState = {
-    movies: {},
-    loading: true,
-    film: {}
-}
-
-
+  movies: {},
+  loading: true,
+  film: {},
+};
 
 const movieSlice = createSlice({
-    name: 'movies',
-    initialState,
-    reducers:{},
-    extraReducers: {
-        [fetchAsyncMovies.pending] : (state) => {
-            console.log('Movies Pending');
-            return {...state, loading: true}
-        },
-        [fetchAsyncMovies.fulfilled]: (state, {payload}) => {
-            console.log('Movies fetched');
-            return {...state, movies: payload, loading: false}
-        },
-        [fetchAsyncMovies.rejected] : () => {
-            console.log('Movies Rejected');
-        },
+  name: "movies",
+  initialState,
+  reducers: {},
+  extraReducers: {
+    [fetchAsyncMovies.pending]: (state) => {
+      console.log("Movies Pending");
+      return { ...state, loading: true };
+    },
+    [fetchAsyncMovies.fulfilled]: (state, { payload }) => {
+      console.log("Movies fetched");
+      return { ...state, movies: payload, loading: false };
+    },
+    [fetchAsyncMovies.rejected]: () => {
+      console.log("Movies Rejected");
+    },
+  },
+});
 
-    }
-})
-
-
-export const getMovies = (state) => state.movies.movies
-export const getLoading = (state) => state.movies.loading
-export default movieSlice.reducer
+export const getMovies = (state) => state.movies.movies;
+export const getLoading = (state) => state.movies.loading;
+export default movieSlice.reducer;
